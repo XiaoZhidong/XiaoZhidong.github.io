ea036fd7e9ec0ddf5085bcf7dd1b981ad2afefd3
XiaoZhidong <Xiaozhid@mail2.sysu.edu.cn>
2014-02-19 20:56:57 +0800

    导航

---
+++ js/show.js
@@ -0,0 +1,7 @@
+function show(obj)
+{	
+	document.getElementById("li1").style.color = "black";
+	document.getElementById("li2").style.color = "black";
+	document.getElementById("li3").style.color = "black";
+	obj.style.color = "#000099";
+}
