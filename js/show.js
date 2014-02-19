387e23a42b75ecf6139f0c8ca4c2752c55a204ae
XiaoZhidong <Xiaozhid@mail2.sysu.edu.cn>
2014-02-20 00:15:11 +0800

    导航加强

--- js/show.js
+++ js/show.js
@@ -1,7 +1,22 @@
 function show(obj)
 {	
 	document.getElementById("li1").style.color = "black";
+	document.getElementById("li1").style.borderColor = "#CCFF99";
+	document.getElementById("li1").style.background = "#CCFF33";
+	document.getElementById("li1").style.borderWidth = "1px 0px"
+	
 	document.getElementById("li2").style.color = "black";
+	document.getElementById("li2").style.borderColor = "#CCFF99";
+	document.getElementById("li2").style.background = "#CCFF33";
+	document.getElementById("li2").style.borderWidth = "1px 0px";
+	
 	document.getElementById("li3").style.color = "black";
+	document.getElementById("li3").style.borderColor = "#CCFF99";
+	document.getElementById("li3").style.background = "#CCFF33";
+	document.getElementById("li3").style.borderWidth = "1px 0px";
+	
 	obj.style.color = "#000099";
+	obj.style.borderColor = "#663333";
+	obj.style.background = "#00ff00";
+	obj.style.borderWidth = "thick 0px";
 }
